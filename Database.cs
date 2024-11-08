1a04419b1aacc295946b44d216d8121b5c7cd444
박상호 Sangho Park <ps040211@gmail.com>
2024-11-08 18:43:34 +0900

    Config 생성과 데이터베이스 연결 시스템
    Config 에서 데이터베이스와 연결하기 위한 정보를 저장할 수 있게 코드를 수정하고,
    appsettings.json 를 생성할 수 있도록 코드를 작성합니다.
    
    추가 적으로 Program.cs 에서는 앞에서 커밋될 코드가 호출되어 있습니다.

---
+++ Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace LukeVanillaAPI
+{
+    public class Database
+    {
+        private readonly string connectionString;
+
+        public Database(IConfiguration configuration)
+        {
+            var server = configuration["Database:Server"];
+            var port = configuration["Database:Port"];
+            var database = configuration["Database:Database"];
+            var user = configuration["Database:User"];
+            var password = configuration["Database:Password"];
+
+            connectionString = $"Server={server};Port={port};Database={database};User ID={user};Password={password};";
+        }
+
+        public async Task<MySqlConnection> GetConnectionAsync()
+        {
+            var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,42 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using LukeVanillaAPI.discord;
+using LukeVanillaAPI;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        var bot = new DiscordBot();
-        bot.RunAsync().GetAwaiter().GetResult();
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var bot = serviceProvider.GetRequiredService<DiscordBot>();
+        await bot.RunAsync();
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        var configuration = Config.InitializeConfiguration();
+
+        services.AddSingleton<IConfiguration>(configuration);
+        
+        // DiscordSocketClient를 올바른 Configuration으로 초기화
+        services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.Guilds |
+                             GatewayIntents.GuildMessages |
+                             GatewayIntents.MessageContent
+        }));
+
+        services.AddSingleton<Database>();
+        services.AddSingleton<AuthChat>();
+        services.AddSingleton<DiscordBot>();
     }
 }
